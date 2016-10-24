10621f4193fad25e13817fb100295ad3a55e0fba
Andrea Faciane and Jesse McKay <andrea@>
2016-10-24 10:23:07 -0700

    get forms working and assign variables to each input to put into arrays.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 
+$(document).ready(function() {
+  $("form#questions").submit(function(event) {
+    var colorInput = $("#favColor").val();
+    var carInput = $("#carBrand").val();
+    var bandInput = $("#favBand").val();
+    var questionArray = [colorInput, carInput, bandInput];
+    questionArray.pop();
+    alert(questionArray);
+
+    event.preventDefault();
+  });
+
+});
